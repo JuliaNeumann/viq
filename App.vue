fa1d8e1ca9243836034e32036318976752d092a5
JuliaNeumann <jn.julianeumann@gmail.com>
2019-11-01 14:15:17 +0100

    keyboard avoid view, messages as list

--- App.vue
+++ App.vue
@@ -1,65 +1,80 @@
 <template>
-  <view class="container">
-    <text class="text">Wo bist du?</text>
-    <text v-if="displayedAnswer" class="text text--answer">{{ displayedAnswer }}</text>
+  <KeyboardAvoidingView class="container" behavior="height" enabled>
+    <text v-for="(message, index) in messages"
+          :key="index"
+          class="text"
+          :class="'text--' + message.type">
+          {{ message.text }}
+    </text>
     <view class="input-container">
-      <text-input class="answer-input" v-model="answer"/>
+      <text-input class="answer-input" v-model="userInput"/>
       <button color="#0E494E" title="Senden" :on-press="send" />
     </view>
-  </view>
+  </KeyboardAvoidingView>
 </template>
 
 <script>
     export default {
         data: function() {
             return {
-                answer: '',
-                displayedAnswer: ''
+                userInput: '',
+                messages: [
+                  {
+                    type: 'god',
+                    text: 'Wo bist du?'
+                  }
+                ]
             };
         },
         methods: {
           send: function() {
-            this.displayedAnswer = this.answer;
-            this.answer = '';
+            this.messages.push({
+              type: 'user',
+              text: this.userInput
+            });
+            this.userInput = '';
           }
         }
     }
 </script>
 
 <style>
-.container {
-  background-color: #74999C;
-  align-items: flex-start;
-  justify-content: flex-end;
-  flex: 1;
-  padding: 10;
-}
+  .container {
+    background-color: #74999C;
+    align-items: flex-start;
+    justify-content: flex-end;
+    flex: 1;
+    padding: 10;
+  }
 
-.input-container {
-  flex-direction: row;
-}
+  .input-container {
+    flex-direction: row;
+  }
 
-.text {
-  background-color: #CFD6D7;
-  border-color: #013034;
-  border-width: 1;
-  border-radius: 5;
-  margin-bottom: 10;
-  padding: 10;
-}
+  .text {
+    border-color: #013034;
+    border-width: 1;
+    border-radius: 5;
+    margin-bottom: 10;
+    padding: 10;
+  }
 
-.text--answer {
-  background-color: white;
-  align-self: flex-end;
-}
+  .text--god {
+    background-color: #CFD6D7;
+  }
+  
+  .text--user {
+    background-color: white;
+    align-self: flex-end;
+  }
 
-.answer-input {
-  flex-grow: 1;
-  height: 40;
-  width: 100;
-  background-color: white;
-  border-color: #013034;
-  border-width: 1;
-  padding: 10;
-}
+  .answer-input {
+    flex-grow: 1;
+    height: 40;
+    width: 100;
+    background-color: white;
+    border-color: #013034;
+    border-width: 1;
+    padding: 10;
+  }
 </style>
